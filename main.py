5d9b5ff1fee062266398f84f981bfad617ebe3e3
Adham Ahmed <adham.ahmed@bld.ai>
2023-12-13 15:34:23 +0200

    Refactor: less color statement + modify the UI and the way of calc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from datetime import datetime
+from datetime import datetime, timedelta
 from decimal import Decimal
 from tkinter import messagebox, ttk, filedialog
 from tktimepicker import AnalogPicker, AnalogThemes
@@ -12,46 +12,50 @@ from zk import ZK
 
 def range_date(attendaces, start_date, end_date):
     result = []
-    # string to date time
-    start_date = datetime.strptime(start_date, "%Y-%m-%d")
-    end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    #  datetime.date to datetime.datetime
+    start_date = datetime.combine(start_date, datetime.min.time())
+    end_date = datetime.combine(end_date, datetime.max.time())
     for attendance in attendaces:
         if attendance.timestamp >= start_date and attendance.timestamp <= end_date:
             result.append(attendance)
     return result
 
 
-def blow_it(start_date, end_date, ip):
+def blow_it(
+    start_date, end_date, ip, data_file_location, shift_length, shift_start_time
+):
     conn = None
     zk = ZK(
         ip,
         port=4370,
-        timeout=5,
+        timeout=1,
         password=0,
         force_udp=False,
         ommit_ping=False,
     )
     dict = {}
+    attendances = []
     try:
         conn = zk.connect()
         conn.disable_device()
         attendances = conn.get_attendance()
         conn.enable_device()
-        final = range_date(attendances, start_date, end_date)
-
-        for i in final:
-            if i.user_id in dict:
-                dict[i.user_id].append(i.timestamp)
-            else:
-                dict[i.user_id] = []
-                dict[i.user_id].append(i.timestamp)
     except Exception as e:
         print("Process terminate : {}".format(e))
+        return False
     finally:
         if conn:
             conn.disconnect()
 
-    wb = openpyxl.load_workbook("Employee.xlsx", data_only=True)
+    final = range_date(attendances, start_date, end_date)
+    for i in final:
+        if i.user_id in dict:
+            dict[i.user_id].append(i.timestamp)
+        else:
+            dict[i.user_id] = []
+            dict[i.user_id].append(i.timestamp)
+
+    wb = openpyxl.load_workbook(data_file_location, data_only=True)
     sheet = wb.active
     sheet.sheet_view.rightToLeft = True
     Emplyees = {}
@@ -136,6 +140,7 @@ def blow_it(start_date, end_date, ip):
         wb.create_sheet(title=str(id))
         sheet = wb[str(id)]
         sheet.sheet_view.rightToLeft = True
+        # سعر اليوم - سعر الاضافي - الوقت الاضافي - الاجمالي
         sheet.append(
             [
                 "الكود",
@@ -143,49 +148,109 @@ def blow_it(start_date, end_date, ip):
                 "حضور",
                 "انصراف",
                 "الوقت",
-                "السعر",
-                "الاجمالي",
+                "سعر الساعة",
+                "اجمالي اليوم",
+                "الاضافي",
+                "اجمالي الاضافي",
                 "الملاحظات",
             ]
         )
+
         sheet.append(
-            [id, Emplyees[id][0], "", "", "", Emplyees[id][2], Emplyees[id][2] * 12, ""]
+            # id,name,attendance,leave,time,price,total,extra,extraTotal,notes
+            [
+                id,  # id
+                Emplyees[id][0],  # name
+                "",  # attendance
+                "",  # leave
+                "",  # time
+                Emplyees[id][2],  # price
+                Decimal(int(Emplyees[id][2])) * Decimal(shift_length),  # total
+                "",  # extra
+                "",  # extraTotal
+                "",  # notes
+            ]
         )
         timeSpent = 0
         totalRow = 0
         for i in range(0, len(Emplyees[id][3]) - 1, 2):
-            daySec = (Emplyees[id][3][i + 1] - Emplyees[id][3][i]).total_seconds()
+            # we want to calculate the if he exceed the start time by 20 min we will subtract 2 hours
+            # if he exceed time by 30 min we will subtract 4 hours
+            # start = 8:10 am if he exceed 8:20 am we will subtract 2 hours
+            # if he exceed 8:20 am we will subtract 4 hours
+            # if he came before 8:10 am and after 8:00 am we will not subtract any thing
+            notes = ""
+
+            shift_start_datetime = datetime.combine(datetime.today(), shift_start_time)
+            shift_end_datetime = shift_start_datetime + timedelta(
+                hours=int(shift_length)
+            )
+            daySec = (
+                Emplyees[id][3][i + 1] - min(Emplyees[id][3][i], shift_end_datetime)
+            ).total_seconds()
+
+            if (
+                Emplyees[id][3][i].time()
+                > (shift_start_datetime + timedelta(minutes=10)).time()
+                and Emplyees[id][3][i].time()
+                < (shift_start_datetime + timedelta(minutes=20)).time()
+            ):
+                daySec -= 2 * 60 * 60
+                notes = "تأخير اكثر من 10 دقائق"
+            elif (
+                Emplyees[id][3][i].time()
+                > (shift_start_datetime + timedelta(minutes=20)).time()
+            ):
+                daySec -= 4 * 60 * 60
+                notes = "تأخير اكثر من 20 دقيقة"
+
+            # Assuming shift_length is an integer representing hours
+            shift_end_time = (
+                shift_start_datetime + timedelta(hours=int(shift_length))
+            ).time()
+
+            # Assuming Emplyees[id][3][i + 1] is a datetime.datetime object
+            overTime = (
+                datetime.combine(datetime.today(), Emplyees[id][3][i + 1].time())
+                - datetime.combine(datetime.today(), shift_end_time)
+            ).total_seconds()
+
+            overTime = max(overTime, 0)
+
+            if overTime:
+                notes += "\n عمل وقت اضافي" + " " + str((overTime / 60) / 60)
+
+            daySec = max(daySec, 0)
             timeSpent += daySec
             secondPrice = (Emplyees[id][2] / 60) / 60
             sheet.append(
                 [
-                    "",
-                    "",
-                    Emplyees[id][3][i],
-                    Emplyees[id][3][i + 1],
-                    (Emplyees[id][3][i + 1] - Emplyees[id][3][i]),
-                    Emplyees[id][2],
-                    Decimal(daySec) * Decimal(secondPrice),
-                    "",
+                    "",  # id
+                    "",  # name
+                    Emplyees[id][3][i],  # attendance
+                    Emplyees[id][3][i + 1],  # leave
+                    (daySec / 60) / 60,  # time
+                    Emplyees[id][2],  # price
+                    Decimal(daySec) * Decimal(secondPrice),  # total
+                    (overTime / 60) / 60,  # extra
+                    Decimal(overTime) * Decimal(secondPrice),  # extraTotal
+                    notes,  # notes
                 ]
             )
             totalRow += 1
-        sheet.append(
-            ["", "", "", "", "", "", Decimal(timeSpent) * Decimal(secondPrice), ""]
-        )
-        sheet["G" + str(totalRow + 3)].font = FontForSum
-        sheet["G" + str(totalRow + 3)].fill = PatternFill(
-            start_color="D3D3D3", end_color="D3D3D3", fill_type="solid"
-        )
-        sheet["A1"].font = FontObj
-        sheet["B1"].font = FontObj
-        sheet["C1"].font = FontObj
-        sheet["D1"].font = FontObj
-        sheet["E1"].font = FontObj
-        sheet["F1"].font = FontObj
-        sheet["G1"].font = FontObj
-        sheet["H1"].font = FontObj
-        # modify each cell size
+        # sheet.append(
+        #     ["", "", "", "", "", "", Decimal(timeSpent) * Decimal(secondPrice), ""]
+        # )
+        # sheet["G" + str(totalRow + 3)].font = FontForSum
+        # sheet["G" + str(totalRow + 3)].fill = PatternFill(
+        #     start_color="D3D3D3", end_color="D3D3D3", fill_type="solid"
+        # )
+        colered_cell = ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1", "J1"]
+        for cell in colered_cell:
+            sheet[cell].font = FontObj
+            sheet[cell].fill = PatternFill(
+                start_color="D3D3D3", end_color="D3D3D3", fill_type="solid"
+            )
         sheet.column_dimensions["A"].width = 10
         sheet.column_dimensions["B"].width = 30
         sheet.column_dimensions["C"].width = 30
@@ -194,33 +259,13 @@ def blow_it(start_date, end_date, ip):
         sheet.column_dimensions["F"].width = 30
         sheet.column_dimensions["G"].width = 30
         sheet.column_dimensions["H"].width = 30
-        # change background color for first row to gray
-        sheet["A1"].fill = PatternFill(
-            start_color="D3D3D3", end_color="D3D3D3", fill_type="solid"
-        )
-        sheet["B1"].fill = PatternFill(
-            start_color="D3D3D3", end_color="D3D3D3", fill_type="solid"
-        )
-        sheet["C1"].fill = PatternFill(
-            start_color="D3D3D3", end_color="D3D3D3", fill_type="solid"
-        )
-        sheet["D1"].fill = PatternFill(
-            start_color="D3D3D3", end_color="D3D3D3", fill_type="solid"
-        )
-        sheet["E1"].fill = PatternFill(
-            start_color="D3D3D3", end_color="D3D3D3", fill_type="solid"
-        )
-        sheet["F1"].fill = PatternFill(
-            start_color="D3D3D3", end_color="D3D3D3", fill_type="solid"
-        )
-        sheet["G1"].fill = PatternFill(
-            start_color="D3D3D3", end_color="D3D3D3", fill_type="solid"
-        )
-        sheet["H1"].fill = PatternFill(
-            start_color="D3D3D3", end_color="D3D3D3", fill_type="solid"
-        )
+        sheet.column_dimensions["I"].width = 30
+        sheet.column_dimensions["J"].width = 50
 
-    wb.save("Data/" + start_date + "+" + end_date + ".xlsx")
+    start_time = start_date.strftime("%Y-%m-%d")
+    end_time = end_date.strftime("%Y-%m-%d")
+    wb.save("Data/" + start_time + "+" + end_time + ".xlsx")
+    return True
 
 
 def submit_dates():
@@ -235,10 +280,20 @@ def submit_dates():
     event = time[2]
     time_to_datetime = datetime.strptime(f"{hrs}:{mins} {event}", "%I:%M %p")
     print(time_to_datetime.time())
-    
-    blow_it(start_date, end_date, ip)
-    
-    if start_date and end_date:
+
+    isDone = blow_it(
+        start_date,
+        end_date,
+        ip,
+        data_file_location,
+        # to int because it's string
+        int(shift_length),
+        time_to_datetime.time(),
+    )
+    if not isDone:
+        messagebox.showerror("خطأ", "الرجاء التأكد من الاتصال بالجهاز !")
+        return
+    if start_date and end_date and isDone:
         messagebox.showinfo(
             "تم عمل التقرير", f"تاريخ البدء: {start_date}\nتاريخ الانتهاء: {end_date}"
         )
